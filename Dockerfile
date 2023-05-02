cdaa73f8594f5c446c450365884e29251e7083a9
Luisitodelcastillo <castilloluis166@gmail.com>
2023-05-02 10:10:31 +0200

    feat: #25 Dockerfile v1

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,13 @@
 # Partim d’una imatge mínima de node 18.12.1 LTS https://hub.docker.com/_/node
-FROM node:lts-alpine3.15
+FROM node:lts-alpine3.16
+
+
 # Indicar el creador
 LABEL maintainer="Codebiting - JDH"
+
+
+# Instalem wget
+RUN apk update && apk add wget
 # Creem un directori per l'aplicacio i descarreguem el projecte
 RUN mkdir -p /usr/src/app
 # Establim el directori de treball
@@ -14,8 +20,9 @@ RUN npm install
 RUN ls -l /usr/src/app
 # Comprobació de que el contenidor funciona correctament
 HEALTHCHECK  --interval=5m --timeout=3s \
-             CMD wget --no-verbose --tries=1 \
+             CMD wget -q --no-verbose --tries=1 \
              --spider http://localhost:8080/v1/healthcheck || exit 1
+
 # Exposem el port de l'aplicacio
 EXPOSE 8080
 # Establim les variables d'entorn
--- test/routes/v1/healthcheck_spec.js
+++ test/routes/v1/healthcheck_spec.js
@@ -1,31 +1,45 @@
 /**
  * Created by jordi on 04/03/2023.
- * 
+ *
  * To run the test:
  * 1. Start the server `npm start`
  * 2. Execute test `mocha test/routes/v1/healthcheck_spec.js --timeout 2000`
  */
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-const expect = require('chai').expect;
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+const expect = require("chai").expect;
+
 
 chai.use(chaiHttp);
 
-const URL= 'http://localhost:8080/v1';
 
-describe('API Healthcheck ',()=>{
-  it('should return status ok', (done) => {
-    chai.request(URL)
-    .get('/healthcheck')
-    .end(function(err, res) {
-      //console.log(res.body);
-      expect(res).to.have.status(200);
-      expect(res.body).to.have.status('OK');
-      expect(res.body.data).to.be.an('array').that.eql([{ result: 'ok' }]);
-      expect(res.body.errors).to.be.an('array').that.eql([]);
-      done();
-    });
+const URL = "http://localhost:8080/v1";
+
+
+describe("API Healthcheck ", () => {
+  it("should return status ok when the container is running", (done) => {
+    chai
+      .request(URL)
+      .get("/healthcheck")
+      .end(function (err, res) {
+        //console.log(res.body);
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.status("OK");
+        expect(res.body.data).not.to.be.an("array");
+        expect(res.body.data).to.be.eql({
+          id: 1,
+          clientId: 1,
+          code: "C1",
+          description: "Container 1",
+          width: 1,
+          length: 1,
+          height: 1,
+          maxWeight: 1,
+        });
+        expect(res.body.requestId).to.be.an("string");
+        expect(res.body.errors).to.be.an("array");
+        expect(res.body.errors).to.be.an("array").that.eql([]);
+        done();
+      });
   });
 });
-
- 
