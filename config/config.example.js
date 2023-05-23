29c978e225aa84fd2f72ef1492516e60a2a8571e
jordidh <jordi.dh@gmail.com>
2023-05-23 11:15:25 +0200

    feat:afegim un fitxer de configuració d'exemple

---
+++ config/config.example.js
@@ -0,0 +1,12 @@
+module.exports = {
+    client: "TEST",
+    service: "onion-cargo-loading-service",
+    db: {
+      host: 'localhost',
+      port: 3306,
+      database: 'cargo_loading',
+      user: 'root',
+      password: 'root',
+      connectionLimit: 10
+    }
+}
