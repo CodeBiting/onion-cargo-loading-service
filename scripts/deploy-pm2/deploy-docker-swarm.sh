5f11a91e2706ffd561de0d4dc57aa27e203cfba1
Arcedo <marcarcedoalvaro25@gmail.com>
2023-08-31 15:50:26 +0200

    feat: #36 deploy-docker fixes

--- scripts/deploy-pm2/deploy-docker-swarm.sh
+++ scripts/deploy-pm2/deploy-docker-swarm.sh
@@ -12,19 +12,19 @@ printf "\n\nConfiguration data:"
 #read -p "  Usuari de GitHub: " githubUsername
 #read -sp "  Contrasenya:" githubPassword
 printf "\n Data for MySQL\n"
-read -p "  User: " myuser
-read -sp "  Passoword: " mypass
+#read -p "  User: " myuser
+read -sp "  Root Passoword: " mypass
 # Comprovate if the evniroment variables are empty (no username or password)
-if [ -z "$myuser" ]; then
-  myuser=default
-  printf "\n -- No user entry\n  ---> DEFAULT USERNAME: default"
-fi
+#if [ -z "$myuser" ]; then
+#  myuser=default
+#  printf "\n -- No user entry\n  ---> DEFAULT USERNAME: default"
+#fi
 if [ -z "$mypass" ]; then
   mypass=Mypass123
   printf "\n -- No password entry.\n  ---> DEFAULT PASSWORD: Mypass123"
 fi
 # Export values for MySQL docker image
-export MYSQL_USER=myuser
+#export MYSQL_USER=myuser
 export MYSQL_PASSWORD=mypass
 
 printf "\n** GET PROJECT REPOSITORY **\n"
@@ -42,7 +42,7 @@ echo '      host: "mysql",' >> config/config.js
 echo '      port: 3306,' >> config/config.js 
 echo '      database: "cargo_loading",' >> config/config.js 
 echo "      user: 'root'," >> config/config.js 
-echo "      password: 'Mypass123'," >> config/config.js 
+echo "      password: '$mypass'," >> config/config.js 
 echo '      connectionLimit: 10' >> config/config.js 
 echo '    }' >> config/config.js 
 echo '}' >> config/config.js 
@@ -57,5 +57,5 @@ sudo apt install docker-compose -y
 
 printf "\n** Building/Starting docker-compose **\n"
 docker-compose up -d
-
+docker-compose
 printf "\n*** END ***\n"
