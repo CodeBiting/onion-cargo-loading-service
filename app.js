39e08f5da0a4ed99381afeb092679247ceee9346
Arcedo <marcarcedoalvaro25@gmail.com>
2023-10-04 16:30:36 +0200

    fix errors

--- app.js
+++ app.js
@@ -100,7 +100,14 @@ app.use(async (req, res, next) => {
   // Save the requestId or create a new one if not exists
   req.requestId = requestId || uniqid();
   // Call the next function in the middleware
-
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Request-Method', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Cambia esto según la URL de tu aplicación Nuxt
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  //res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  //res.setHeader('Access-Control-Allow-Headers', 'Content-Type, application/json');
+  
   next();
 });
 
--- routes/v1/container.js
+++ routes/v1/container.js
@@ -259,6 +259,7 @@ router.post('/smallest/:clientId', async function (req, res, next) {
   try {
     if (redisContainers) {
       const reply = await redisContainers.get(`containersFrom${req.params.clientId}`);
+      let prova =JSON.parse(reply);
       if (reply) {
         containers = JSON.parse(reply);
       } else {
@@ -366,7 +367,7 @@ router.post('/', async function (req, res, next) {
 
   try {
     containerCreated = await containerService.postContainer(req.body);
-    if (containerCreated && redisContainers) await redisContainers.del(`containerFrom${req.body.clientId}`);
+    if (containerCreated && redisContainers) await redisContainers.del(`containersFrom${req.body.clientId}`);
   } catch (ex) {
     logger.error(
       `${API_NAME}: [${req.method}] ${req.originalUrl}: reqId=${req.requestId} : ${ex}`
@@ -434,7 +435,7 @@ router.put('/:id', async function (req, res, next) {
     const containerNewData = req.body;
 
     containerUpdated = await containerService.putContainer(id, containerNewData);
-    if (containerUpdated && redisContainers) await redisContainers.del(`containerFrom${containerUpdated.clientId}`);
+    if (containerUpdated && redisContainers) await redisContainers.del(`containersFrom${containerUpdated.clientId}`);
     if (containerUpdated === undefined) {
       logger.error(
         `${API_NAME}: [${req.method}] ${req.originalUrl}: reqId=${req.requestId} : Container not found`
@@ -507,7 +508,7 @@ router.delete('/:id', async function (req, res, next) {
     const id = req.params.id;
 
     containerDeleted = await containerService.deleteContainer(id);
-    if (containerDeleted && redisContainers) await redisContainers.del(`containerFrom${containerDeleted.clientId}`);
+    if (containerDeleted && redisContainers) await redisContainers.del(`containersFrom${containerDeleted.clientId}`);
     if (containerDeleted === undefined) {
       logger.error(
         `${API_NAME}: [${req.method}] ${req.originalUrl}: reqId=${req.requestId} : Container not found`
@@ -581,7 +582,7 @@ router.put('/:id/delete', async function (req, res, next) {
 
   try {
     containerDeleted = await containerService.desactivateContainer(req.params.id);
-    if (containerDeleted && redisContainers) await redisContainers.del(`containerFrom${containerDeleted.clientId}`);
+    if (containerDeleted && redisContainers) await redisContainers.del(`containersFrom${containerDeleted.clientId}`);
     if (containerDeleted === undefined) {
       logger.error(
         `${API_NAME}: [${req.method}] ${req.originalUrl}: reqId=${req.requestId} : Container not found`
